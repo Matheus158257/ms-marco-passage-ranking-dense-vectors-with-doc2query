405e6142f1c0fb072f64443c1f15b2806d2c7f0d
Matheus158257 <matheus.sasso17@gmail.com>
2020-06-08 21:50:25 -0300

    inclusao strip

--- read_ms_marco.py
+++ read_ms_marco.py
@@ -10,7 +10,7 @@ import os
 def load_qrels(path,encoding="cp1252"):
   """Loads qrels into a dict of key: query id, value: set of relevant doc ids."""
   qrels = collections.defaultdict(set)
-  with open(path, encoding=encoding, newline='',quotechar='"') as f:
+  with open(path, encoding=encoding, newline='') as f:
     for i, line in enumerate(f):
       query_id, _, doc_id, relevance = line.rstrip().split('\t')
       if int(relevance) >= 1:
@@ -23,10 +23,10 @@ def load_qrels(path,encoding="cp1252"):
 def load_queries(path,encoding="cp1252"):
   """Loads queries into a dict of key: query id, value: query text."""
   queries = {}
-  with open(path, encoding=encoding, newline='',quotechar='"') as f:
+  with open(path, encoding=encoding, newline='') as f:
     for i, line in enumerate(f):
       query_id, query = line.rstrip().split('\t')
-      queries[query_id] = query
+      queries[query_id] = query.strip('"')
       if i % 100000 == 0:
         print('Loading queries {}'.format(i))
   return queries
@@ -35,10 +35,10 @@ def load_queries(path,encoding="cp1252"):
 def load_collection(path,encoding="cp1252"):
   """Loads tsv collection into a dict of key: doc id, value: doc text."""
   collection = {}
-  with open(path,encoding=encoding, newline='',quotechar='"') as f:
+  with open(path,encoding=encoding, newline='') as f:
     for i, line in enumerate(f):
       doc_id, doc_text = line.rstrip().split('\t')
-      collection[doc_id] = doc_text.replace('\n', ' ')
+      collection[doc_id] = doc_text.replace('\n', ' ').strip('"')
       if i % 1000000 == 0:
         print('Loading collection, doc {}'.format(i))
 
@@ -47,10 +47,10 @@ def load_collection(path,encoding="cp1252"):
 def load_doc2query(path,encoding="cp1252"):
   """Loads txt queries from doc2query into a dict of key: doc id, value: doc query."""
   collection = {}
-  with open(path, encoding=encoding, newline='',quotechar='"') as f:
+  with open(path, encoding=encoding, newline='') as f:
     for i, line in enumerate(f):
       doc_text = line.rstrip()
-      collection[str(i)] = doc_text.replace('\n', ' ')
+      collection[str(i)] = doc_text.replace('\n', ' ').strip('"')
       if i % 1000000 == 0:
         print('Loading doc2query, doc {}'.format(i))
 
@@ -60,10 +60,10 @@ def load_doc2query(path,encoding="cp1252"):
 def load_triple(path,encoding="cp1252", max_i=None):
     """Load triple tsv into python dict."""
     triples = collections.defaultdict(set)
-    with open(path, encoding=encoding, newline='',quotechar='"') as f:
+    with open(path, encoding=encoding, newline='') as f:
         for i, line in enumerate(f):
             qid, pid, nid = line.rstrip().split('\t')
-            triples[i] = qid, pid, nid
+            triples[i] = qid.strip('"'), pid.strip('"'), nid.strip('"')
             if i % 100000 == 0:
                 print('Loading triple {}'.format(i))
             if max_i != None and i > max_i:
